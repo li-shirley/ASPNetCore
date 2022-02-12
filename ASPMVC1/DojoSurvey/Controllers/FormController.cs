b020d33d1ffd10a2bbdffa1548ae1795dbbb976a
Shirley Li <lishirley@me.com>
2022-02-11 16:05:05 -0800

    updated to satisfy "Dojo Survey with Model" reqs

--- ASPMVC1/DojoSurvey/Controllers/FormController.cs
+++ ASPMVC1/DojoSurvey/Controllers/FormController.cs
@@ -1,25 +1,21 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using DojoSurvey.Models;
+
 namespace DojoSurvey.Controllers
 {
     public class FormController : Controller
     {
-        [HttpGet]
-        [Route("")]
+        [HttpGet("")]
         public ViewResult Index()
         {
             return View();
         }
 
-        [HttpPost]
-        [Route("result")]
-        public IActionResult Result(string Name, string Location, string Language, string Comment)
+        [HttpPost("result")]
+        public IActionResult Result(Survey yourSurvey)
         {
-            ViewBag.Name = Name;
-            ViewBag.Location = Location;
-            ViewBag.Language = Language;
-            ViewBag.Comment = Comment;
-            return View();
+            return View("Result", yourSurvey);
         }
     }
 }
